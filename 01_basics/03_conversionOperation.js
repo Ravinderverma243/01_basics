b72f02265e10050673bbdf57e1c96464d8d140a5
Ravinder Verma <150565734+Ravinderverma243@users.noreply.github.com>
2023-11-14 08:48:22 +0000

    data type summary

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -1,3 +1,31 @@
-let someNumber = 22
-let stringNumber = string(someNumber)
-console.log(stringNumber);
+// let someNumber = 22
+// console.log(someNumber)
+
+// operation
+/*console.log(3**5)
+console.log(2/5)
+console.log(2*3)
+console.log(2%3)
+let value = 5;
+let nagvalue = -value;
+console.log(nagvalue)*/
+// let str1 = "Hello"
+// let str2 =  "Ravi"
+// let str3 = str1 + " " + str2
+// console.log(str3);
+/*************let a = 3;
+a++;
+console.log(a)**************/
+// console.log(2>1)
+// console.log(2<1)
+// console.log("ravi">=1)
+// console.log(null>=1)
+// console.log(null>=0)
+ let X = {
+    Name: "Ravi",
+    Class: 9,
+ }
+ console.log(X)
+
+ let Y = ["Ravi", "Bansi", "Parveen"]
+ console.log(Y)
---
+++ 01_basics/04_datatype_summary.js
@@ -0,0 +1,11 @@
+let a = null
+console.log(typeof a)
+
+let b = true;
+console.log(typeof b)
+let s = ["flowers", "leafs"]
+console.log(typeof s)
+let myfunc = function(){
+    console.log("Hay Ravi")
+}
+console.log(myfunc)
