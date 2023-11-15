07e3958be20e208c2e0b2e73288926b198d14503
Ravinder Verma <150565734+Ravinderverma243@users.noreply.github.com>
2023-11-15 08:06:18 +0000

    explore strings

---
+++ 01_basics/05_string.js
@@ -0,0 +1,20 @@
+const name = "ravi"
+const Id = "hello"
+// console.log(`${Id} How are you ${name}`)
+const gameName = new String(`ravi-rk`)
+// console.log(gameName.__proto__)
+// console.log(gameName.length)
+// console.log(gameName.sup())
+// console.log(gameName.toUpperCase())
+
+// console.log(gameName.charAt(2))
+// console.log(gameName.indexOf(`a`))
+
+// const newString = gameName.substring(3, 5)
+// console.log(newString)
+// const anotherString = gameName.slice(-6, 3)
+// console.log(anotherString)
+const url = `https//:Ravi.com/Ravi%20Verma`
+console.log(url.replace(`%20`, `=`))
+console.log(url.includes(`ver`))
+console.log(url.includes(`Ver`))
